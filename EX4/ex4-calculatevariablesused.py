3da9ac7c93bbe70d5d09235837d959056068ffd0
Brandon Dedmore <dedmore@gmail.com>
2013-10-08 12:12:19 -0700

    commit

---
+++ EX4/ex4-calculatevariablesused.py
@@ -0,0 +1,10 @@
+# This is a program to calculate how many bits there are in 1 Gigabyte.
+print "This programs calculates how many bits there are in 1 Gigabyte" 
+print "1 GB is 1024 MB"
+i = 1024
+print "1 MB is 1024 KB"
+x = 1024
+print "1 KB is 1024 B"
+j = 1024
+print "1 Byte is 8 Bits"
+print "There are", i * x * j * 8, "Bits in a Gigabyte"
