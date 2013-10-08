61b5fc6c420de6f6a848d8acbb07a37ba76b8261
Brandon Dedmore <dedmore@gmail.com>
2013-10-08 16:07:22 -0700

    commit

---
+++ EX7/ex7-commented.py
@@ -0,0 +1,26 @@
+# Prints string text on screen
+print "Mary had a little lamb."
+# Prints string text on screen with string inside
+print "Its fleece was white as %s." % 'snow'
+# Prints string text on screen
+print "And everywhere that Mary went."
+# Prints the string "." ten times
+print "." * 10
+
+# Variable define section
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Prints variables in secession on the screen forming two words
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
