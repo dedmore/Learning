1eecca5a5e6c4140f0cd66f6c1597416560ce214
Brandon Dedmore <dedmore@gmail.com>
2013-10-08 16:46:35 -0700

    commit

---
+++ EX9/ex9-commented.py
@@ -0,0 +1,17 @@
+# Defines days with string value
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# Defines months with string value including new line information
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# Prints "Here are the days: " followed by the string value of days
+print "Here are the days: ", days
+# Prints " Here are the months: " followed by the string value of months including the new line information
+print "Here are the months: ", months
+
+# Prints multiple lines of text until the triple double quotes are reached
+print """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
